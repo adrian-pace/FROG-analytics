3b006f561e6a75558a7d568721651f0802fce053
Adrian Pace <adrian.pace@epfl.ch>
2017-12-12 19:18:47 +0100

    time analysis, clustering and correlation between metrics #15

--- testing stuff/frog contact.py
+++ testing stuff/frog contact.py
@@ -1,10 +1,10 @@
 import requests
 
-r = requests.post("http://127.0.0.1:5000",json={'pad_names':["editor2"]})
+r = requests.post("http://127.0.0.1:5000", json={'pad_names': ["editor2"]})
 print(r)
 
 r = requests.get("http://127.0.0.1:5000")
 print(r.text)
 
-r = requests.post("http://127.0.0.1:5000",json={'regex':"^/ac-textarea/default"})
-    ...: print(r)
+r = requests.post("http://127.0.0.1:5000", json={'regex': "^/ac-textarea/default"})
+print(r)
